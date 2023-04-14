3343ddbed7e4370ff7fa45a03e0f86ed9b7f0975
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-14 09:26:09 +0100

    Get the user's input for the number of games

--- index.js
+++ index.js
@@ -31,9 +31,23 @@ function playRockPaperScissors(bestOf = 5){
     let score = 0;
     let gamesRemaining = bestOf;
     while(gamesRemaining > 0){
-        let userInput = prompt("Please input Rock, Paper or Scissors.", "");
+        let userInput = prompt(`Game ${bestOf - gamesRemaining + 1} of ${bestOf}\nPlease input Rock, Paper or Scissors.`, "");
         userInput = userInput.toLowerCase()
-
+        let computerChoice = getComputerChoice();
+        
+        if(userInput === computerChoice){ console.log(`It's a draw! You both selected ${userInput}!`); }
+        if(
+            (userInput === 'rock' && computerChoice === 'scissors') ||
+            (userInput === 'paper' && computerChoice === 'rock') ||
+            (userInput === 'scissors' && computerChoice === 'paper')
+        ){ console.log(`You won! Your ${userInput} beat the computer's ${computerChoice}!`); }
+        if(
+            (userInput === 'rock' && computerChoice === 'paper') ||
+            (userInput === 'paper' && computerChoice === 'scissors') ||
+            (userInput === 'scissors' && computerChoice === 'rock')
+        ){ console.log(`You lost... Your ${userInput} was beaten by the computer's ${computerChoice}.`); }
+        
+        console.log("That selection is invalid. Please choose either 'Rock', 'Paper' or 'Scissors'.");
 
         gamesRemaining--;
     }
