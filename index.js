bbfde2d11a1097104063624da79711a6943c1dbb
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-14 09:13:03 +0100

    Play rock paper scissors on the command line

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3)
     switch(rand){
-        case 0: return "Rock";
-        case 1: return "Paper";
-        case 2: return "Scissors";
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissors";
     }
+}
+
+function playRockPaperScissors(userInput = "") {
+    let computerChoice = getComputerChoice();
+    if(typeof userInput !== "string"){ return "That input is not a string. Please choose either 'Rock', 'Paper' or 'Scissors'."; }
+    userInput = userInput.toLowerCase()
+    
+    if(userInput === computerChoice){ return `It's a draw! You both selected ${userInput}!`; }
+    if(
+        (userInput === 'rock' && computerChoice === 'scissors') ||
+        (userInput === 'paper' && computerChoice === 'rock') ||
+        (userInput === 'scissors' && computerChoice === 'paper')
+    ){ return `You won! Your ${userInput} beat the computer's ${computerChoice}!`; }
+    if(
+        (userInput === 'rock' && computerChoice === 'paper') ||
+        (userInput === 'paper' && computerChoice === 'scissors') ||
+        (userInput === 'scissors' && computerChoice === 'rock')
+    ){ return `You lost... Your ${userInput} was beaten by the computer's ${computerChoice}.`; }
+    
+    return "That selection is invalid. Please choose either 'Rock', 'Paper' or 'Scissors'.";
 }
